fc74ac72261671904e7eda4e3f77af77904cd449
dancer1325 <alfredotic0809@gmail.com>
2023-08-27 12:23:23 +0200

    doc(documentation): add reference.Enums constant member cases

--- packages/documentation/copy/en/reference/Enums/Enums.ts
+++ packages/documentation/copy/en/reference/Enums/Enums.ts
@@ -113,3 +113,82 @@ enum BooleanLikeHeterogeneousEnum {
 }
 console.log("BooleanLikeHeterogeneousEnum.No " + BooleanLikeHeterogeneousEnum.No);
 console.log("BooleanLikeHeterogeneousEnum.Yes " + BooleanLikeHeterogeneousEnum.Yes);
+
+
+//                            --    enum's members constant or computed   --
+// 1. constant member
+// 1.1 first member, non-initialized
+enum E {
+  X,
+}
+// 1.2 non-first members, non initialized nothing
+enum E1 {
+  X,
+  Y,
+  Z,
+}
+// 1.3 non-first members, non initialized, preceded by a numeric constant
+enum E2 {
+  A = 1,
+  B,
+  C,
+}
+// 1.4 constant enum expression
+// 1.4.1 literal enum expression
+// 1.4.1.1 literal string
+enum literalString {
+  First="FIRST",
+  Second="SECOND",
+}
+// 1.4.1.2 literal numeric
+enum literalNumber {
+  First=0,
+  Second=1,
+}
+// 1.4.2 reference to another enum's constant member
+enum referenceToAnotherEnumConstant{
+  AnotherFirst= literalString.First,
+  AnotherSecond = literalString.Second
+}
+// 1.4.3 parenthesized constant enum expression
+enum Colors {
+  Red = "RED",
+  Green = "GREEN",
+  Blue = "BLUE",
+}
+console.log("(Colors.Red) " + (Colors.Red));
+// 1.4.4 unary operators applied to constant enum expression
+enum withUnaryOperators {
+  UnaryPlus= +2,
+  UnaryMinus= -8,
+  BitwiseNotOperator= ~5
+}
+console.log("withUnaryOperators.UnaryPlus " + withUnaryOperators.UnaryPlus);
+console.log("withUnaryOperators.UnaryMinus " + withUnaryOperators.UnaryMinus);
+console.log("withUnaryOperators.BitwiseNotOperator " + withUnaryOperators.BitwiseNotOperator);
+// 1.4.5 binary operators applied to constant enum expressions
+enum withBinaryOperators{
+  Add= 2+2,
+  Minus= 7-8,
+  Multiply = 2*4,
+  Divide= 4/2,
+  Remainder = 5%2,
+  LeftShifting= 5 << 2,
+  RightShifting= 5 >> 2,
+  UnsignedRightSift= -5 >>>2,
+  BitwiseAnd= 5 & 3,
+  BitwiseOr= 5 | 3,
+  BitwiseXor= 5^3,
+}
+console.log("withBinaryOperators.Add " + withBinaryOperators.Add);
+console.log("withBinaryOperators.Minus " + withBinaryOperators.Minus);
+console.log("withBinaryOperators.Divide " + withBinaryOperators.Divide);
+console.log("withBinaryOperators.Remainder " + withBinaryOperators.Remainder);
+console.log("withBinaryOperators.LeftShifting " + withBinaryOperators.LeftShifting);
+console.log("withBinaryOperators.RightShifting " + withBinaryOperators.RightShifting);
+console.log("withBinaryOperators.UnsignedRightSift " + withBinaryOperators.UnsignedRightSift);
+console.log("withBinaryOperators.BitwiseOr " + withBinaryOperators.BitwiseOr);
+console.log("withBinaryOperators.BitwiseXor " + withBinaryOperators.BitwiseXor);
+
+
+
