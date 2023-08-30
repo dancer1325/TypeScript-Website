919f8f95ad60388ea02d744d94c705c548b4fd88
dancer1325 <alfredotic0809@gmail.com>
2023-08-30 12:27:31 +0200

    doc(documentation): add declarationFiles.ByExample question commentary

--- packages/documentation/copy/en/declaration-files/By Example/By Example.ts
+++ packages/documentation/copy/en/declaration-files/By Example/By Example.ts
@@ -2,6 +2,7 @@
 // If you don't use declare keyword, as next line -> compiler complains, since it's not defined in a .ts file
 // function f1(): { a: number; b: string };
 // Next line, compiler doesn't complain   -- but it should be declared in some part, not in a .ts file --
+// TODO: How to make it work at runtime, implementing the function?
 declare function f1(): { a: number; b: string };
 
 const result = f1();
