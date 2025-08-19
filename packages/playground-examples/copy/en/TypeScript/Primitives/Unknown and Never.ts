dbd649f3f20f2bc99bd5865e890ab7889e44345d
dancer1325 <alfredotic0809@gmail.com>
2025-08-19 09:49:46 +0200

    doc(): clear notes

--- packages/playground-examples/copy/en/TypeScript/Primitives/Unknown and Never.ts
+++ packages/playground-examples/copy/en/TypeScript/Primitives/Unknown and Never.ts
@@ -1,46 +1,29 @@
-// Unknown
-
-// Unknown is one of those types that once it clicks, you
-// can find quite a lot of uses for it. It acts like a sibling
-// to the any type. Where any allows for ambiguity - unknown
-// requires specifics.
-
-// A good example would be in wrapping a JSON parser. JSON
-// data can come in many different forms and the creator
-// of the json parsing function won't know the shape of the
-// data - the person calling that function should.
-
-const jsonParser = (jsonString: string) => JSON.parse(jsonString);
+// 1. Unknown
+// 1.1 any
+const jsonParser = (jsonString: string) => JSON.parse(jsonString);    // if you do NOT specify -> any
 
 const myAccount = jsonParser(`{ "name": "Dorothea" }`);
 
 myAccount.name;
 myAccount.email;
 
-// If you hover on jsonParser, you can see that it has the
-// return type of any, so then does myAccount. It's possible
-// to fix this with generics - but it's also possible to fix
-// this with unknown.
-
+// 1.2 unknown
 const jsonParserUnknown = (jsonString: string): unknown => JSON.parse(jsonString);
 
 const myOtherAccount = jsonParserUnknown(`{ "name": "Samuel" }`);
 
+// 1.2.1 ❌WITHOUT checking the type -> you can NOT use it❌
 myOtherAccount.name;
 
-// The object myOtherAccount cannot be used until the type has
-// been declared to TypeScript. This can be used to ensure
-// that API consumers think about their typing up-front:
-
+// 1.2.2 type assertion BEFORE using it
 type User = { name: string };
 const myUserAccount = jsonParserUnknown(`{ "name": "Samuel" }`) as User;
 myUserAccount.name;
 
-// Unknown is a great tool, to understand it more read these:
-// https://mariusschulz.com/blog/the-unknown-type-in-typescript
-// https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-0.html#new-unknown-top-type
 
-// Never
+
+
+// 2. Never
 
 // Because TypeScript supports code flow analysis, the language
 // needs to be able to represent when code logically cannot
